e499340a3215c2a5ffe35a7c81774d6f0e25024f
Mcdonalds Trump <whdmschd2@gmail.com>
2022-02-19 04:56:25 +0900

    Reafactor: 검색 함수 분리

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, MouseEventHandler, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { NutrientsListType } from '../../api/getNutrientsList';
 
@@ -13,6 +13,11 @@ const SearchBar = ({ defaultNutrientsList, changeNutrientsList, changeCurrentKye
   const [searchName, setSearchName] = useState<string>('');
   const [isClientSearching, setIsClientSearching] = useState<boolean>(false);
 
+  const getFilterNutrients = (searchValue: string): NutrientsListType[] | undefined => {
+    const filteredNutrients = defaultNutrientsList?.filter((nutrients) => nutrients.name.includes(searchValue));
+    return filteredNutrients;
+  };
+
   const handleSubmitSearchValue = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     const formTarget = e.target as HTMLFormElement;
@@ -21,9 +26,9 @@ const SearchBar = ({ defaultNutrientsList, changeNutrientsList, changeCurrentKye
     setIsClientSearching(false);
     changeCurrentKyeword(value); // PR 충돌
     if (!value || value === ' ') return;
-    const filteredNutrients = defaultNutrientsList?.filter((nutrients) => nutrients.name.includes(value));
+    const filteredNutrients = getFilterNutrients(value);
     if (filteredNutrients?.length) {
-      changeNutrientsList(filteredNutrients); //이 함수 안에 setState값이 들어있어서 일단 밖으로
+      changeNutrientsList(filteredNutrients);
     } else {
       console.log(`"${value}"에 해당하는 제품을 찾을 수 없습니다.`);
     }
@@ -37,7 +42,7 @@ const SearchBar = ({ defaultNutrientsList, changeNutrientsList, changeCurrentKye
     } else {
       setIsClientSearching(true);
     }
-    const filteredNutrients = defaultNutrientsList?.filter((nutrients) => nutrients.name.includes(event.target.value));
+    const filteredNutrients = getFilterNutrients(event.target.value);
     if (filteredNutrients) {
       if (filteredNutrients.length > 10) {
         setNutrientsList(filteredNutrients.slice(0, 10));
@@ -47,15 +52,9 @@ const SearchBar = ({ defaultNutrientsList, changeNutrientsList, changeCurrentKye
     }
   };
 
-  const clientSearching = () => {
-    setIsClientSearching(true);
-  };
-
   const liClickChangeList = (e: React.MouseEvent<HTMLLIElement>) => {
     const listElement = e.target as HTMLLIElement;
-    const filteredNutrients = defaultNutrientsList?.filter((nutrients) =>
-      nutrients.name.includes(listElement.innerText),
-    );
+    const filteredNutrients = getFilterNutrients(listElement.innerText);
     if (filteredNutrients) {
       changeNutrientsList(filteredNutrients);
       setIsClientSearching(false);
