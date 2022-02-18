91ccd83b1da3246c36788720778fb607535d7800
choi2601 <byeonghyeonchoe55@gmail.com>
2022-02-18 15:09:19 +0900

    Refactor: parseCateories 관련 로직 분리

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -2,36 +2,37 @@ import { useEffect, useState } from 'react';
 import type { NutrientsListType } from '../api/getNutrientsList';
 
 type CheckBoxInfoType = {
-    checked: boolean, cnt: number
-}
+  checked: boolean;
+  cnt: number;
+};
 
 const useCategories = (nutrientsList: NutrientsListType[] | null) => {
-    const [initialNutrientsList, setInitialNutientsList] = useState<NutrientsListType | null>(null);
-    const [checkboxInfo, setCheckboxInfo] = useState<[string, CheckBoxInfoType][] | null>(null);
+  const [initialNutrientsList, setInitialNutientsList] = useState<NutrientsListType | null>(null);
+  const [checkboxInfo, setCheckboxInfo] = useState<[string, CheckBoxInfoType][] | null>(null);
 
-    const filterCheckboxInfo = (parseCategories: NutrientsListType[]) => {
-        const filtedCheckboxInfo = new Map();
+  const filterCheckboxInfo = (nutrientsList: NutrientsListType[]) => {
+    const parseCategories = nutrientsList.filter((nutrients) => nutrients.brand);
+    const filtedCheckboxInfo = new Map();
 
-        for(const x of parseCategories) {
-            const { brand } = x;
-            if(!filtedCheckboxInfo.has(brand)) filtedCheckboxInfo.set(brand, { checked: false, cnt: 1 });
-            else filtedCheckboxInfo.set(brand, {...filtedCheckboxInfo.get(brand), cnt: filtedCheckboxInfo.get(brand).cnt + 1});
-        }
-
-        return filtedCheckboxInfo;
+    for (const prodcut of parseCategories) {
+      const { brand } = prodcut;
+      if (!filtedCheckboxInfo.has(brand)) filtedCheckboxInfo.set(brand, { checked: false, cnt: 1 });
+      else
+        filtedCheckboxInfo.set(brand, { ...filtedCheckboxInfo.get(brand), cnt: filtedCheckboxInfo.get(brand).cnt + 1 });
     }
 
-    useEffect(() => {
-        if(nutrientsList) {
-            const parseCategories = nutrientsList.filter((nutrients) => nutrients.brand);
-            
-            const newCheckboxInfo = Array.from(filterCheckboxInfo(parseCategories));
-            
-            setCheckboxInfo(newCheckboxInfo);
-        }
-    }, [nutrientsList]);
+    return filtedCheckboxInfo;
+  };
 
-    return { checkboxInfo };
-}
+  useEffect(() => {
+    if (nutrientsList) {
+      const newCheckboxInfo = Array.from(filterCheckboxInfo(nutrientsList));
+
+      setCheckboxInfo(newCheckboxInfo);
+    }
+  }, [nutrientsList]);
+
+  return { checkboxInfo };
+};
 
 export default useCategories;
